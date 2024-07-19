a6dbb27c657e34f452d39edb395a52a718df39ce
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-07-19 14:01:09 +0530

    AddedAuctionMicroservice

---
+++ AuctionService/Dtos/CreateAuctionDto.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionService.Dtos
+{
+    public class CreateAuctionDto
+    {
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+
+        [Required]
+        public int Mileage { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int ReservePrice { get; set; }
+
+        [Required]
+        public DateTime AuctionEnd { get; set; }
+    }
+}
